78c3b11527a43c2118052d23450fd2d398f71d19
skhazri <khazri.sarah@gmail.com>
2019-09-14 21:59:30 -0400

    add service test

---
+++ src/test/java/ca/khazri/superHero/service/MissionServiceTest.java
@@ -0,0 +1,151 @@
+package ca.khazri.superHero.service;
+
+import ca.khazri.superHero.dto.request.MissionRequest;
+import ca.khazri.superHero.model.Mission;
+import ca.khazri.superHero.repository.MissionRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+@RunWith(SpringRunner.class)
+
+public class MissionServiceTest {
+
+//    @Autowired
+//    private MissionService missionService;
+
+    @MockBean
+    private MissionRepository missionRepository;
+
+    @Mock
+    private MissionService missionService;
+
+@Before
+public void setup(){
+    // With this call to initMocks we tell Mockito to process the annotations
+    MockitoAnnotations.initMocks(this);
+}
+
+
+    @Test
+    public void findAllMissions ( ) {
+
+//        // when
+        List<Mission> missions = new ArrayList<Mission> ();
+        missions.add(new Mission (1L, "mission1", false, false ));
+        missions.add(new Mission (2L,"mission2", false, false ));
+        missions.add(new Mission (3L,"mission3", false, true ));
+        when(missionService.findAllMissions()).thenReturn(missions);
+
+        // assert
+        List<Mission> foundMissions = missionService.findAllMissions();
+        // assert
+        assertEquals(foundMissions.size(), 3);
+        assertThat(foundMissions.get(0).getId()).isEqualTo(1);
+        assertThat(foundMissions.get(0).getMissionName()).isEqualTo("mission1");
+        assertThat(foundMissions.get(0).getIsCompleted ()).isEqualTo(false);
+        assertThat(foundMissions.get(0).getIsDeleted ()).isEqualTo(false);
+
+        assertThat(foundMissions.get(1).getId()).isEqualTo(2);
+        assertThat(foundMissions.get(1).getMissionName()).isEqualTo("mission2");
+        assertThat(foundMissions.get(1).getIsCompleted ()).isEqualTo(false);
+        assertThat(foundMissions.get(1).getIsDeleted ()).isEqualTo(false);
+
+        assertThat(foundMissions.get(2).getId()).isEqualTo(3);
+        assertThat(foundMissions.get(2).getMissionName()).isEqualTo("mission3");
+        assertThat(foundMissions.get(2).getIsCompleted ()).isEqualTo(false);
+        assertThat(foundMissions.get(2).getIsDeleted ()).isEqualTo(true);
+
+    }
+
+    @Test
+    public void findMissionById ( ) {
+        // given
+        Mission mission = new Mission (1L, "mission", false, false );
+
+        // when
+        when(missionService.findMissionById(mission.getId ())).thenReturn(mission);
+
+        // assert
+        Mission foundMission = missionService.findMissionById(1L);
+        assertThat(foundMission.getId()).isEqualTo(1);
+        assertThat(foundMission.getMissionName()).isEqualTo("mission");
+        assertThat(foundMission.getIsCompleted ()).isEqualTo(false);
+        assertThat(foundMission.getIsDeleted ()).isEqualTo(false);
+    }
+
+
+
+    @Test
+    public void createMission ( ) {
+
+        // given
+        MissionRequest missionRequest = new MissionRequest("mission",false, false);
+        Mission mission = new Mission(1L, "mission",false, false);
+
+        // when
+        when(missionService.createMission(missionRequest)).thenReturn(mission);
+
+        // then
+        Mission createdMission = missionService.createMission(missionRequest);
+        // assert
+
+        assertThat(createdMission.getId()).isEqualTo(1);
+        assertThat(createdMission.getMissionName()).isEqualTo("mission");
+        assertThat(createdMission.getIsCompleted ()).isEqualTo(false);
+        assertThat(createdMission.getIsDeleted ()).isEqualTo(false);
+    }
+
+    @Test
+    public void updateMission ( ) {
+
+        // given
+        MissionRequest missionRequest = new MissionRequest("mission ",false, false);
+        Mission mission = new Mission(1L, "missionUpdated",true, true);
+
+        // when
+        when(missionService.updateMission (missionRequest,1L)).thenReturn(mission);
+
+        // assert
+        Mission updatedMission = missionService.updateMission (missionRequest,1L);
+        assertThat(updatedMission.getId()).isEqualTo(1);
+        assertThat(updatedMission.getMissionName()).isEqualTo("missionUpdated");
+        assertThat(updatedMission.getIsCompleted ()).isEqualTo(true);
+        assertThat(updatedMission.getIsDeleted ()).isEqualTo(true);
+    }
+
+    @Test
+    public void softDeleteMission ( ) {
+        // given
+        Mission mission = new Mission(1L, "mission",false, false);
+        Mission softDeletedMission = new Mission(1L, "mission",false, true);
+
+        // when
+        when(missionService.softDeleteMission(mission.getId())).thenReturn(softDeletedMission);
+
+        // assert
+        Mission deleteMission = missionService.softDeleteMission(mission.getId());
+        assertThat(deleteMission.getId()).isEqualTo(1);
+        assertThat(deleteMission.getMissionName()).isEqualTo("mission");
+        assertThat(deleteMission.getIsCompleted ()).isEqualTo(false);
+        assertThat(deleteMission.getIsDeleted ()).isEqualTo(true);
+    }
+}
+
+//
---
+++ src/test/java/ca/khazri/superHero/service/SuperHeroServiceTest.java
@@ -0,0 +1,214 @@
+package ca.khazri.superHero.service;
+
+import ca.khazri.superHero.dto.request.MissionRequest;
+import ca.khazri.superHero.dto.request.SuperHeroMissionRequest;
+import ca.khazri.superHero.dto.request.SuperHeroRequest;
+import ca.khazri.superHero.dto.response.ApiResponse;
+import ca.khazri.superHero.dto.response.ErrorResponse;
+import ca.khazri.superHero.dto.response.SuccessResponse;
+import ca.khazri.superHero.model.Mission;
+import ca.khazri.superHero.model.SuperHero;
+import ca.khazri.superHero.repository.MissionRepository;
+import ca.khazri.superHero.repository.SuperHeroRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+public class SuperHeroServiceTest {
+
+    @MockBean
+    private MissionRepository missionRepository;
+
+    @MockBean
+    private SuperHeroRepository superHeroRepository;
+
+    @Mock
+    private MissionService missionService;
+
+    @Mock
+    private SuperHeroService superHeroService;
+
+    @Before
+    public void setup(){
+        // With this call to initMocks we tell Mockito to process the annotations
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findAllSuperHeroes ( ) {
+
+        // given
+        SuperHero superHero1 = new SuperHero (1L,"FirstName1","LastName1","SuperHeroName1");
+        SuperHero superHero2 = new SuperHero (2L,"FirstName2","LastName2","SuperHeroName2");
+        SuperHero superHero3 = new SuperHero (3L,"FirstName3","LastName3","SuperHeroName3");
+
+
+        List heroes = new ArrayList<Mission> (  );
+        heroes.add(superHero1);
+        heroes.add(superHero2);
+        heroes.add(superHero3);
+
+        // when
+        when(superHeroService.findAllSuperHeroes ()).thenReturn(heroes);
+
+        // assert
+        List<SuperHero> foundSuperHeroes = superHeroService.findAllSuperHeroes ();
+        // assert
+        assertEquals(foundSuperHeroes.size (),3);
+        assertThat(foundSuperHeroes.get(0).getId()).isEqualTo(1);
+        assertThat(foundSuperHeroes.get(0).getFirstName()).isEqualTo("FirstName1");
+        assertThat(foundSuperHeroes.get(0).getLastName()).isEqualTo("LastName1");
+        assertThat(foundSuperHeroes.get(0).getSuperheroName()).isEqualTo("SuperHeroName1");
+        assertThat(foundSuperHeroes.get(1).getId()).isEqualTo(2);
+        assertThat(foundSuperHeroes.get(1).getFirstName()).isEqualTo("FirstName2");
+        assertThat(foundSuperHeroes.get(1).getLastName()).isEqualTo("LastName2");
+        assertThat(foundSuperHeroes.get(1).getSuperheroName()).isEqualTo("SuperHeroName2");
+        assertThat(foundSuperHeroes.get(2).getId()).isEqualTo(3);
+        assertThat(foundSuperHeroes.get(2).getFirstName()).isEqualTo("FirstName3");
+        assertThat(foundSuperHeroes.get(2).getLastName()).isEqualTo("LastName3");
+        assertThat(foundSuperHeroes.get(2).getSuperheroName()).isEqualTo("SuperHeroName3");
+    }
+
+    @Test
+    public void findSuperHeroById ( ) {
+
+        // given
+        SuperHero superHero = new SuperHero (1L,"FirstName","LastName","SuperHeroName");
+
+        // when
+        when(superHeroService.findSuperHeroById (superHero.getId ())).thenReturn(superHero);
+
+
+        // then
+        SuperHero foundHero = superHeroService.findSuperHeroById(superHero.getId ());
+
+        // assert
+        assertThat(foundHero.getId()).isEqualTo(1);
+        assertThat(foundHero.getFirstName ()).isEqualTo("FirstName");
+        assertThat(foundHero.getLastName ()).isEqualTo("LastName");
+        assertThat(foundHero.getSuperheroName ()).isEqualTo("SuperHeroName");
+    }
+
+    @Test
+    public void createSuperHero ( ) {
+
+        // given
+        SuperHeroRequest superheroRequest = new SuperHeroRequest("FirstName","LastName", "SuperHeroName");
+        SuperHero superHero = new SuperHero(1L, "FirstName","LastName", "SuperHeroName");
+
+        // when
+        when(superHeroService.createSuperHero (superheroRequest)).thenReturn(superHero);
+
+        // then
+        SuperHero createdSuperHero = superHeroService.createSuperHero(superheroRequest);
+        // assert
+
+        assertThat(createdSuperHero.getId()).isEqualTo(1);
+        assertThat(createdSuperHero.getFirstName()).isEqualTo("FirstName");
+        assertThat(createdSuperHero.getLastName()).isEqualTo("LastName");
+        assertThat(createdSuperHero.getSuperheroName()).isEqualTo("SuperHeroName");
+    }
+
+    @Test
+    public void updateSuperHero ( ) {
+
+        // given
+        SuperHeroRequest superheroRequest = new SuperHeroRequest("FirstName","LastName", "SuperHeroName");
+        SuperHero superHero = new SuperHero(1L, "updatedFirstName","updatedLastName", "updatedSuperHeroName");
+
+        // when
+        when(superHeroService.updateSuperHero (superheroRequest,1L)).thenReturn(superHero);
+
+        // then
+        SuperHero updateSuperHero = superHeroService.updateSuperHero(superheroRequest,1L);
+        //assert
+        assertThat(updateSuperHero.getId()).isEqualTo(1);
+        assertThat(updateSuperHero.getFirstName()).isEqualTo("updatedFirstName");
+        assertThat(updateSuperHero.getLastName()).isEqualTo("updatedLastName");
+        assertThat(updateSuperHero.getSuperheroName()).isEqualTo("updatedSuperHeroName");
+    }
+
+    @Test
+    public void deleteSuperHero ( ) {
+
+        // given
+        SuperHero superHero = new SuperHero(1L, "FirstName","LastName", "SuperHeroName");
+        SuccessResponse successResponse = new SuccessResponse ("Super Hero is deleted");
+        // when
+        when(superHeroService.deleteSuperHero (superHero.getId())).thenReturn(successResponse);
+
+        // then
+        ApiResponse apiResponse = superHeroService.deleteSuperHero(superHero.getId());
+        //assert
+        assertThat(apiResponse.getSuccess()).isEqualTo(true);
+        assertThat(apiResponse.getMessages ().get ( 0 )).isEqualTo("Super Hero is deleted");
+    }
+
+    @Test
+    public void addSuperHeroMission ( ) {
+        // given
+        SuperHero superHero = new SuperHero(1L, "FirstName","LastName", "SuperHeroName");
+        Mission mission = new Mission(1L, " mission",false, false);
+        SuperHeroMissionRequest superheroMissionRequest = new SuperHeroMissionRequest(superHero.getId(), mission.getId());
+        SuccessResponse successResponse = new SuccessResponse( "Mission added to Superhero");
+
+      //   when
+        when(superHeroService.addSuperHeroMission (superheroMissionRequest)).thenReturn(successResponse);
+        //then
+        ApiResponse apiResponse = superHeroService.addSuperHeroMission (superheroMissionRequest);
+
+        // assert
+        assertThat(apiResponse.getSuccess()).isEqualTo(true);
+        assertThat(apiResponse.getMessages().get ( 0 )).isEqualTo("Mission added to Superhero");
+    }
+
+    @Test
+    public void removeSuperHeroMission ( ) {
+
+        //Success Example
+        // given
+        SuperHero superHero = new SuperHero(1L, "FirstName","LastName", "SuperHeroName");
+        Mission mission = new Mission(1L, " mission",false, false);
+        SuccessResponse successResponse = new SuccessResponse( "Mission removed from Superhero");
+
+        // when
+        when(superHeroService.removeSuperHeroMission (superHero.getId (),mission.getId ())).thenReturn(successResponse);
+
+        // then
+        ApiResponse apiResponse = superHeroService.removeSuperHeroMission (superHero.getId (),mission.getId ());
+        //assert
+        assertThat(apiResponse.getSuccess()).isEqualTo(true);
+        assertThat(apiResponse.getMessages().get ( 0 )).isEqualTo("Mission removed from Superhero");
+
+        //Failure Example
+        // given
+        Mission completedMission = new Mission(1L, " mission",true, false);
+        ApiResponse errorResponse = new ErrorResponse ( "Unable to remove a completed mission");
+//
+        // when
+        when(superHeroService.removeSuperHeroMission (superHero.getId (),mission.getId ())).thenReturn(errorResponse);
+//
+        // then
+        ApiResponse FailedApiResponse = superHeroService.removeSuperHeroMission (superHero.getId (),mission.getId ());
+        assertThat(FailedApiResponse.getSuccess()).isEqualTo(false);
+        assertThat(FailedApiResponse.getMessages().get ( 0 )).isEqualTo("Unable to remove a completed mission");
+    }
+}
