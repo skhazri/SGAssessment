abeb5e47bcf650e1f1cb58281773eae8b350fbad
skhazri <khazri.sarah@gmail.com>
2019-09-14 21:58:25 -0400

    add json mapper

---
+++ src/main/java/ca/khazri/superHero/utils/Utils.java
@@ -0,0 +1,19 @@
+package ca.khazri.superHero.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utils {
+
+    /*
+     * converts a Java object into JSON representation
+     */
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper ().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
